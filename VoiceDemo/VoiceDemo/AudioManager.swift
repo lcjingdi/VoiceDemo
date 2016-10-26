d629721219c5b5252577686c389a0462b652384d
lcjingdi <lcjingdi@gmail.com>
2016-10-27 02:52:45 +0800

    增加了音乐播放类

--- VoiceDemo/VoiceDemo/AudioManager.swift
+++ VoiceDemo/VoiceDemo/AudioManager.swift
@@ -10,9 +10,47 @@ import UIKit
 import AVFoundation
 
 class AudioManager: NSObject {
-
-    /// : 播放音效
     
+    static var musics = [String: AnyObject]()
+    
+    // MARK: - 音乐控制
+    /// 播放音乐
+    class func playMusic(filename: String!) -> Bool {
+        
+        if let player = musics[filename] as? AVAudioPlayer {
+            print("音频时长:\(player.duration) 音频路径:\(player.url)")
+            if !player.isPlaying {
+                return player.play()
+            }
+        } else {
+            guard let url = Bundle.main.url(forResource: filename, withExtension: nil) else { return false }
+            let player = try! AVAudioPlayer(contentsOf: url)
+            print("音频时长:\(player.duration) 音频路径:\(player.url)")
+            guard player.prepareToPlay() else { return false }
+            musics[filename] = player
+            print(musics)
+            if !player.isPlaying {
+                return player.play(atTime: 10)
+            }
+        }
+        return true
+    }
+    /// 暂停音乐
+    class func pauseMusic(filename: String!) {
+        if let player = musics[filename] as? AVAudioPlayer {
+            if player.isPlaying {
+                player.pause()
+            }
+        }
+    }
+    /// 停止音乐
+    class func stopMusic(filename: String!) {
+        (musics[filename] as? AVAudioPlayer)?.stop()
+        musics.removeValue(forKey: filename)
+    }
+    
+    // MARK: - 音效控制
+    /// 播放音效
     func playSoundEffect(url: URL ,shake: Bool) {
         
         var soundId = SystemSoundID()
@@ -24,4 +62,5 @@ class AudioManager: NSObject {
         }
         
     }
+    
 }
--- VoiceDemo/VoiceDemo/ViewController.swift
+++ VoiceDemo/VoiceDemo/ViewController.swift
@@ -10,13 +10,14 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+        self.playAudio()
     }
     
     
@@ -24,6 +25,8 @@ class ViewController: UIViewController {
         let url = Bundle.main.url(forResource: "good", withExtension: "mp3")
         AudioManager().playSoundEffect(url: url!, shake: false)
     }
-    
+    func playAudio() {
+        print(AudioManager.playMusic(filename: "xxoo.mp3"))
+    }
 }
 
